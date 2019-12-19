fe946dc517d2ca6306b1e47496c642c988c61791
leehe <lihechn@outlook.com>
2019-12-19 16:34:16 +0800

    modify

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production',
   entry: './src/index.js',
   output: {
-    filename: '[name].[contenthash].js'
+    filename: 'index.[contenthash].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '二哈',
+      template: 'src/assets/index.html'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   }
 };
