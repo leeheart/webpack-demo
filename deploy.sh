80e53cd4c667d0e48a2c7fa17d9beef081f38133
leehe <lihechn@outlook.com>
2019-12-26 16:18:21 +0800

    fix bugs

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ git checkout gh-pages &&
 rm -rf *.html *.js *.css *.png *.jpg &&
 mv dist/* ./ &&
 rm -rf dist
-git add .
-git commit -m 'update'
-git push
+git add . &&
+git commit -m 'update' &&
+git push &&
+git checkout - # 返回上一个分支
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './x.js'
 import png from './assets/1.jpg' //png 为图片路径
 console.log(png)
 
-// const div = document.getElementById('app')
+const div = document.getElementById('app')
 // div.innerHTML = `
 //   <img src='${png}'>
 // `
