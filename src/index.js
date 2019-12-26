435204997da8cdcccde20c77051c8133ced4cadd
leehe <lihechn@outlook.com>
2019-12-26 16:12:54 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import './x.js'
 import png from './assets/1.jpg' //png 为图片路径
 console.log(png)
 
-const div = document.getElementById('app')
-div.innerHTML = `
-  <img src='${png}'>
-`
+// const div = document.getElementById('app')
+// div.innerHTML = `
+//   <img src='${png}'>
+// `
 
 const button = document.createElement('button')
 button.innerText = '懒加载'
